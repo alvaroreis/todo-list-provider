f0ef6bffe28a99e839e4f41821ab8be9da54ba2c
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-07-07 10:04:51 -0300

    feat: add info message

--- lib/app/core/notifier/default_change_notifier.dart
+++ lib/app/core/notifier/default_change_notifier.dart
@@ -4,16 +4,21 @@ abstract class DefaultChangeNotifier extends ChangeNotifier {
   bool _loading = false;
   bool _success = false;
   String? _error;
+  String? _infoMessage;
 
   bool get loading => _loading;
   bool get hasError => null != error;
+  bool get hasInfo => null != infoMessage;
+
   bool get isSuccess => _success;
   String? get error => _error;
+  String? get infoMessage => _infoMessage;
 
   void showLoading() => _loading = true;
   void hideLoading() => _loading = false;
   void success() => _success = true;
-  void setError(String? error) => _error = _error;
+  void setError(String? error) => _error = error;
+  void setInfoMessage(String? message) => _infoMessage = message;
   void showLoadingAndResetState() {
     showLoading();
     resetState();
@@ -21,6 +26,7 @@ abstract class DefaultChangeNotifier extends ChangeNotifier {
 
   void resetState() {
     _error = null;
+    _infoMessage = null;
     _success = false;
   }
 }
